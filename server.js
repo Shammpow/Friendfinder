28354a11ac54a46ce76970b5b3cfdbe6952444e2
Dakota Edwards <drakoden2000@gmail.com>
2018-03-19 10:21:57 -0700

    Added things
    server/html stuff added

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var PORT = process.env.PORT || 8000;
+
+app.use(BodyParser.urlencoded({ extended: false }));
+app.use(BodyParser.json());
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, function() {
+    console.log('App listening on PORT: ' + PORT);
+})
